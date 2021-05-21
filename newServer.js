bca2f8cc1ccde40b7d15e5a26d3903ecd6bb1470
rakesh <chandra.rakeshc@gmail.com>
2021-05-21 15:00:57 +0530

    adding the docker files and mongodb changes

--- newServer.js
+++ newServer.js
@@ -4,8 +4,32 @@ const app = express();
 
 app.use(express.static(process.env.PWD + '/public/images'))
 
+//var mongo = require("mongo")
+
+//var MongoClient = require('mongodb').MongoClient;
+//var url = "mongodb://localhost:27017/firstdb";
+
+/*MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("firstdb");
+  // var rec = { id: "rec123", duration: 45 };	
+  /*dbo.collection("recordings").insert(rec, function(err, res) {
+    if (err) throw err;
+    console.log("inserted data");
+    db.close();
+  });*/ 
+
+  /*dbo.collection("recordings").findOne({}, function(err, result) {
+    if (err) throw err;
+    console.log(result.id);
+    console.log(result.duration);
+    db.close();
+  });*/
+	
+});
+
 app.get('/test1', (req, res) => {
-  
+  console.log('request test1');
   res.sendFile(__dirname+'/index2.html');
 });
 
@@ -22,8 +46,8 @@ app.get('/angTest', (req, res) => {
   res.sendFile(__dirname+'/angularTest.html');
 });
 
-const PORT = 5000;
+const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
+  console.log(`server is running on port ${PORT}`)
 });
